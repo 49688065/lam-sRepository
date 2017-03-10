634489de3b988b6f6064f6748ffb68b78713b0f3
49688065 <49688065@qq.com>
2017-03-10 15:34:35 +0800

    2

--- app/src/main/java/com/lam/jni/MainActivity.java
+++ app/src/main/java/com/lam/jni/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mJni = new MyNdk();
+
     }
 
     public void click(View view) {
